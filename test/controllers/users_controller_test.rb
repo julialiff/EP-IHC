d7be1428231c2b1c27f363e6cdc438db03016408
Julia Litvinoff Justus <julialiff@hotmail.com>
2016-05-09 10:23:13 -0300

    sign up page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,49 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  setup do
-    @user = users(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: { birth_date: @user.birth_date, cellphone: @user.cellphone, city: @user.city, college: @user.college, email: @user.email, name: @user.name, neighbourhood: @user.neighbourhood, occupation: @user.occupation, photo: @user.photo, state: @user.state }
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch :update, id: @user, user: { birth_date: @user.birth_date, cellphone: @user.cellphone, city: @user.city, college: @user.college, email: @user.email, name: @user.name, neighbourhood: @user.neighbourhood, occupation: @user.occupation, photo: @user.photo, state: @user.state }
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
-
-    assert_redirected_to users_path
-  end
 end
