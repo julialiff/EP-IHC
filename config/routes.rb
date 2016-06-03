ad052f78f363b2a883b07dc06d7965cfc0eba4e4
Julia Litvinoff Justus <julialiff@hotmail.com>
2016-06-02 23:23:08 -0300

    header editted

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root   'static_pages#home'
-  get    'about'   => 'static_pages#about'
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get    'about'     => 'static_pages#about'
+  get    'signup'    => 'users#new'
+  get    'signupnh'  => 'night_houses#new'
+  get    'login'     => 'sessions#new'
+  post   'login'     => 'sessions#create'
+  delete 'logout'    => 'sessions#destroy'
 
 
   resources :parties
