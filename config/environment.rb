07b6181aeffdcb5e1dd492262ba4818d5fbd5e66
Julia Litvinoff Justus <julialiff@hotmail.com>
2016-06-18 23:31:38 -0300

    improvements

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+my_date_formats = { :default => '%d/%m/%Y' }
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(my_date_formats)
+
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(my_date_formats)
