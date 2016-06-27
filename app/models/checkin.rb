e3de89713ff04933950eb57f6f937679de178b8c
Julia Litvinoff Justus <julialiff@hotmail.com>
2016-06-27 00:12:02 -0300

    Final adjustments fixes #33 fixes #24 fixes #18 fixes #15 fixes #14 fixes #11 fixes #10

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,4 +1,6 @@
 class Checkin < ActiveRecord::Base
   belongs_to :user
   belongs_to :party
+  validates :user_id, presence: true
+  validates :party_id, presence: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,7 @@ class Photo < ActiveRecord::Base
   belongs_to :party
   mount_uploader :pic, PictureUploader
   default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :party_id, presence: true
+  validates :pic, presence: true
 end
